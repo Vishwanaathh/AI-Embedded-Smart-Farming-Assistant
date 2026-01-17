186855b700c65cc2a57ed42151e7ee9847ffdefb
Vishwanath <140306677+Vishwanaathh@users.noreply.github.com>
2026-01-17 11:26:58 +0530

    Add files via upload

---
+++ crop_recommendation.py
@@ -0,0 +1,29 @@
+
+import joblib
+print("welcome to crop recommendation using LightGBM")
+
+import pandas as pd
+df=pd.read_csv("../Crop_recommendation.csv")
+
+
+final=df[['Nitrogen', 'phosphorus', 'potassium', 'temperature', 'humidity', 'ph',
+       'rainfall', 'label']]
+
+
+from sklearn.model_selection import train_test_split
+X=df[['Nitrogen', 'phosphorus', 'potassium', 'temperature', 'humidity', 'ph',
+       'rainfall']]
+Y=df[['label']]
+x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
+
+
+
+from lightgbm import LGBMClassifier
+
+model=LGBMClassifier(n_estimators=300,
+    learning_rate=0.05,
+    max_depth=8,
+    random_state=42)
+model.fit(x_train,y_train)
+joblib.dump(model, "crop_recommendation_lgbm.pkl")
+
